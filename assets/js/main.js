647ee94c94eeab83547a66413e590c55c791c13f
limilano <limilano@davidson.edu>
2022-09-18 11:39:42 -0400

    all portrait elements now stored and logged in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,11 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+
+const boxes = document.querySelectorAll("#portrait");
+console.log(boxes);
+
+window.addEventListener("mousemove", function(event) {
+  document.getElementById("x-value").textContent = event.x;
+  document.getElementById("y-value").textContent = event.y;
+});
